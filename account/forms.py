01b68e29be46dc5c4c6e6cad01bb859ad773baf5
Kasem414 <kasm0945@gmail.com>
2024-07-30 10:20:14 +0300

    [WIP] Extending user model with a custom profile model and configuring project for media file uploads

--- account/forms.py
+++ account/forms.py
@@ -1,3 +1,4 @@
+from .models import Profile
 from django import forms
 from django.contrib.auth.models import User
 
@@ -16,3 +17,13 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Password don't match")
         return cd ['password2']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','email']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth','photo']
